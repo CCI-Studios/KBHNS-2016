ab080c0eed63415c0f00309a0fa6476b6bf2eb11
Chris Morris <cmorris@ccistudios.com>
2017-01-11 09:35:43 -0500

    fix the team responsive, center the names/titles, and make the names one line

--- sites/all/themes/KBHNS/js/tabs.js
+++ sites/all/themes/KBHNS/js/tabs.js
@@ -1,98 +1,84 @@
 (function($) {
-	
+  $(function() {
 
-	$(function()
-	{	
+    $('.view-team-list .view-content h3').each(function() {
+      $(this).nextUntil('h3').andSelf().wrapAll('<div class="container"></div>');
+    });
 
-        $('.view-team-list .view-content h3').each(function() {
-                 $(this).nextUntil('h3').andSelf().wrapAll('<div class="container"></div>');
-         });
+    $('.view-team .view-content h3').each(function() {
+      $(this).nextUntil('h3').andSelf().wrapAll('<div class="team-container"></div>');
+    });
 
-        $('.view-team .view-content h3').each(function() {
-                 $(this).nextUntil('h3').andSelf().wrapAll('<div class="team-container"></div>');
-         });
+    $('#block-system-main-menu li.expanded a').click(function() {
+      var path = window.location.pathname;
+      var hash = $(this).prop("hash");
+      window.location.href = path + '/' + hash;
+    });
 
-        $('#block-system-main-menu li.expanded a').click(function(){
+    $(".view-ps-sidebar .item-list li a, .view-client-industries-titles .item-list a, .view-profession-type .item-list a").each(function(index) {
+      $(this).attr("href", "#spec" + index.toString());
+      var textValue = $(this).text();
+      textValue = textValue.split(' ').join('_');
+      $(this).addClass(textValue);
+      $(this).parent().addClass('va-slice');
+    });
 
-            var path = window.location.pathname;
-            var hash =$(this).prop("hash");
-            window.location.href=path+'/'+ hash;
+    $('.view-profession-type .item-list a').each(function(index) {
+      var textValue = $(this).text();
+      textValue = textValue.split(' ').join('_');
+      if (textValue == "Associé") {
+        textValue = "Associe";
+      }
+      if ($('body.i18n-fr').length > 0 && textValue == "Consultants") {
+        textValue = "Consultation_fr";
+      }
+      $(this).attr("href", '#' + textValue);
+      $(this).parent().addClass('va-slice');
+    });
 
-        });
+    $about_title = $(".view-about-page-titles .item-list  ul li");
+    $about_title.eq(0).find('a').attr("href", "#mission");
+    $about_title.eq(1).find('a').attr("href", "#history");
+    $about_title.eq(2).find('a').attr("href", "#community");
 
 
-        $(".view-ps-sidebar .item-list li a, .view-client-industries-titles .item-list a, .view-profession-type .item-list a").each(function (index) {
-            $(this).attr("href", "#spec" + index.toString());    
-            var textValue = $(this).text();
-            textValue =textValue.split(' ').join('_');
-            $(this).addClass(textValue);   
-            $(this).parent().addClass('va-slice');
+    $("#block-views-client-industries-detail-block .view-content .content").each(function(index) {
+      var textValue = $(this).find('.views-field-title').text();
+      textValue = textValue.split(' ').join('_');
+      $(this).addClass(textValue);
+    });
 
-        });
+    $(".view-team-list .view-content .container").each(function(index) {
+      var textValue = $(this).find('h3').text();
+      textValue = textValue.split(' ').join('_');
+      if (textValue == "Associé") {
 
-        $('.view-profession-type .item-list a').each(function (index) {
+        textValue = "Associe";
+      }
+      if ($('body.i18n-fr').length > 0 && textValue == "Consultants") {
+        textValue = "Consultation_fr";
+      }
+      $(this).attr("id", textValue);
+      var textValue = $(this).find('.views-field-title').text();
+      textValue = textValue.split(' ').join('_');
+      $(this).addClass(textValue);
+    })
 
-        var textValue = $(this).text();
-        textValue =textValue.split(' ').join('_');
-        if(textValue == "Associé"){
+    $(" .view-about-page-titles .item-list ul,.view-profession-type .item-list ul").tabs();
+    $('.view-ps-sidebar .item-list ul,.view-about-page-titles .item-list ul, .view-profession-type .item-list ul').each(function() {
 
-            textValue = "Associe";
-        }
-        if($('body.i18n-fr').length > 0 && textValue == "Consultants")
-        {
-            textValue = "Consultation_fr";
-        }
-        $(this).attr("href", '#'+textValue);    
-        $(this).parent().addClass('va-slice');
+      var $active, $content, $links = $(this).find('a');
 
-        });
+      $active = $($links.filter('[href="' + location.hash + '"]')[0] || $links[0]);
+      $active.addClass('active');
 
-        $about_title=$(".view-about-page-titles .item-list  ul li");
-        $about_title.eq(0).find('a').attr("href", "#mission"); 
-        $about_title.eq(1).find('a').attr("href", "#history");    
-        $about_title.eq(2).find('a').attr("href", "#community");    
-          
-          
-        $("#block-views-client-industries-detail-block .view-content .content").each(function (index) {
-            
-            var textValue = $(this).find('.views-field-title').text();
-            textValue =textValue.split(' ').join('_');
-            $(this).addClass(textValue);  
-        })
+      $content = $($active[0].hash);
 
-        $(".view-team-list .view-content .container").each(function (index) {
-            var textValue = $(this).find('h3').text();
-            textValue =textValue.split(' ').join('_');
-            if(textValue == "Associé"){
-
-                textValue = "Associe";
-            }
-            if($('body.i18n-fr').length > 0 && textValue == "Consultants")
-            {
-                textValue = "Consultation_fr";
-            }
-            $(this).attr("id", textValue);
-            var textValue = $(this).find('.views-field-title').text();
-            textValue =textValue.split(' ').join('_');
-            $(this).addClass(textValue);  
-        })
-
-        $(" .view-about-page-titles .item-list ul,.view-profession-type .item-list ul").tabs();
-        $('.view-ps-sidebar .item-list ul,.view-about-page-titles .item-list ul, .view-profession-type .item-list ul').each(function(){
-       
-        var $active, $content, $links = $(this).find('a');
-
-      
-        $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
-        $active.addClass('active');
-
-        $content = $($active[0].hash);
-
-        $links.not($active).each(function () {  
+      $links.not($active).each(function() {
         $(this.hash).hide();
-        });
+      });
 
-        $(this).on('click', 'a', function(e){
+      $(this).on('click', 'a', function(e) {
         $active.removeClass('active');
         $content.hide();
         $('.view-team').hide();
@@ -106,8 +92,8 @@
         $content.fadeIn('slow');
 
         e.preventDefault();
+      });
     });
+
   });
-		
-	});
 }(jQuery));
--- sites/all/themes/KBHNS/js/team.js
+++ sites/all/themes/KBHNS/js/team.js
@@ -1,59 +1,53 @@
-(function($){
+(function($) {
+  $(function() {
 
-$(function(){
+    //adding underscore to the href value
+    $('.view-team-list #Partners .views-row,.view-team-list #Associe .views-row, .view-client-industries-detail .views-row,.view-current-opportunities .views-row').each(function(index) {
+      var textValue = $(this).find('a').attr("href");
+      textValue = textValue.replace(/,/g, "");
+      textValue = textValue.split(' ').join('_');
+      $(this).find('a').attr("href", textValue);
+    });
 
-	//adding underscore to the href value
-		$('.view-team-list #Partners .views-row,.view-team-list #Associe .views-row, .view-client-industries-detail .views-row,.view-current-opportunities .views-row').each(function(index){
+    $('.view-team .views-row').each(function(index) {
 
-				var textValue = $(this).find('a').attr("href");  
-				textValue=textValue.replace(/,/g, "");     
-		        textValue =textValue.split(' ').join('_');
-		        $(this).find('a').attr("href",textValue);    
-		});
+      var textValue = $(this).find('.pos_link').attr("id");
+      textValue = textValue.split(' ').join('_');
+      $(this).find('.pos_link').attr("id", textValue);
+    });
 
-		$('.view-team .views-row').each(function(index){
 
-				var textValue = $(this).find('.pos_link').attr("id");    
-		        textValue =textValue.split(' ').join('_');
-		        $(this).find('.pos_link').attr("id",textValue);    
-		});
-			
+    $('.view-team-list #Partners .views-row a, .view-team-list #Associe .views-row a,  .view-team-list .views-row a.view-profile, .view-team-list .views-row a.image-link').click(function(e) {
 
-		$('.view-team-list #Partners .views-row a, .view-team-list #Associe .views-row a,  .view-team-list .views-row a.view-profile, .view-team-list .views-row a.image-link').click(function(e){
+      $('.view-team-list').hide();
+      $('.view-team').show();
+      $('.view-team .team-container').hide();
+      var rowNum = $(this).closest(".views-row").index() - 1;
+      var sectionNum = $(this).closest(".container").index();
 
-			$('.view-team-list').hide();
-			$('.view-team').show();
-			$('.view-team .team-container').hide();
-			var rowNum = $(this).closest(".views-row").index()-1;
-			var sectionNum = $(this).closest(".container").index();
-			
-			$('.view-team .team-container:eq('+sectionNum+')').show();
-			$('.view-team .team-container:eq('+sectionNum+') .views-row:eq('+rowNum+')').fadeIn();
-			
-			var $target = $('.view-team .team-container:eq('+sectionNum+') .views-row:eq('+rowNum+')');
-			var headerOffset = 220;
-			if($(window).width()<767)
-			{
-          headerOffset = 180;
-			}
-			$('html, body').stop().animate({
-					'scrollTop': $target.offset().top-headerOffset
-			}, 400, 'swing');
-			
-			e.preventDefault();
+      $('.view-team .team-container:eq(' + sectionNum + ')').show();
+      $('.view-team .team-container:eq(' + sectionNum + ') .views-row:eq(' + rowNum + ')').fadeIn();
 
-		});
+      var $target = $('.view-team .team-container:eq(' + sectionNum + ') .views-row:eq(' + rowNum + ')');
+      var headerOffset = 220;
+      if ($(window).width() < 767) {
+        headerOffset = 180;
+      }
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - headerOffset
+      }, 400, 'swing');
 
-		$('.view-team .close-icon').click(function(e){
+      e.preventDefault();
+    });
 
-			var textValue = $(this).closest('.views-row a').attr("href")  
-			$('.view-team-list').fadeIn();
-			$('.view-team-list').find(textValue).closest('.container').hide();
-			$(this).parents('.view-team').hide();
-			$(this).parents('.view-team').find('.views-row').hide();
-			e.preventDefault();
-		});
-			
-});
+    $('.view-team .close-icon').click(function(e) {
+      var textValue = $(this).closest('.views-row a').attr("href");
+      $('.view-team-list').fadeIn();
+      $('.view-team-list').find(textValue).closest('.container').hide();
+      $(this).parents('.view-team').hide();
+      $(this).parents('.view-team').find('.views-row').hide();
+      e.preventDefault();
+    });
 
+  });
 })(jQuery);
